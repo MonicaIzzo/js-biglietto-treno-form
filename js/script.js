3056e38171126e37817bc4918d98c0842c7c63a5
MonicaIzzo <info@monicaizzo.net>
2023-05-31 14:20:04 +0200

    add JS steps 0, 1, 2

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ console.log('JS OK')
 /* 
 - 0 Recupero l'elemento da DOM
 - 1 Inizializzo tutte le variabili
-- 2 Chiedo all'utente quanti km che vuole percorrere.
-- 3 Chiedo all'utente quanti anni ha.
+- 2 aggancio un event listener al button di conferma
 - 4 Verifico che i dati inseriti dall'utente
     - SE NON CORRETTI: alert con messaggio ed esco dal programma
     - SE CORRETTI: procedo al punto successivo
@@ -22,102 +21,31 @@ console.log('JS OK')
         OPERAZIONI PRELIMINARI
 ---------------------------------------*/
 
-// # 0
-const PrinceDiscounted = document.getElementById ('prince-discounted');
+// # 0, 1
 
-// # 1
-const discountYoung = 20; // variabile sconto giovani
-const discountOver = 40;  // variabile sconto anziani
-let grossPrice;    // prezzo biglietto lordo
-
-let totalPrince;    // prezzo finale
-
-let rounded;
-let discount;
+// FORM ELEMENTS
+const InputNameSurname = document.getElementById ('InputNameSurname');
+const InputKm = document.getElementById ('InputKm');
+const InputAge = document.getElementById ('InputAge');
+const BtnConfirm = document.getElementById ('confirm');
+const BtnReset = document.getElementById ('reset');
 
 
-/*---------------------------------------
-        MAIN LOGIC
----------------------------------------*/
+// TICKET ELEMENTS
+const ticket = document.getElementById ('ticket');
+
+const NameSurnameElements = document.getElementById ('NameSurname');
+const ticketElements = document.getElementById ('ticket');
+const cabElements = document.getElementById ('cab');
+const codeCPElements = document.getElementById ('codeCP');
+const princeElements = document.getElementById ('prince');
 
 // # 2
-const KMtoTravel = parseInt(prompt('Quanti km vuoi percorrere?').trim());
-console.log(KMtoTravel);
-
-// # 3
-const youAge = parseInt(prompt('Quanti anni hai?').trim());
-console.log(youAge);
-
-
-/*  VALIDAZIONE */
-if (isNaN(KMtoTravel) || (isNaN(youAge)) || youAge <= 0 ||  KMtoTravel <= 0) {
-    alert('Hai inserito valori non validi. Prego riprovare');
-    console.log('Hai inserito valori non validi. Prego riprovare');
-}
-else {
-        // # 4
-    grossPrice =  KMtoTravel * 0.21;
-    console.log(typeof grossPrice);
-
-    // # 5 # 6
-    // nessun sconto
-    if (youAge >= 18 && youAge <= 65)
-        {
-            totalPrince = grossPrice;
-            console.log(totalPrince);
-            discount = 0;
-         document.getElementById ('prince-table').innerHTML = 
-    
-    `
-    <p class="intro"">Ciao, in base alle tue preferenze di viaggio (${KMtoTravel}Km da percorrere) e età anagrafica inserita: (${youAge }) anni,<br>
-    Siamo lieti di proporle la nostra miglior proposta.</p>
-    <hr>
-    <p class="total-prince"> Costo del biglietto: ${grossPrice} €<p>
-    `;
-        
-        }  
-    // sconto young
-    else if (youAge <= 65)
-        {
-            totalPrince = grossPrice - ((grossPrice / 100) * 20);
-            totalPrince = Math.round((totalPrince + Number.EPSILON) * 100) / 100;
-            console.log(totalPrince);
-            discount = 20;
-            document.getElementById ('prince-table').innerHTML = 
-    
-    `
-    <p class="intro"">Ciao, in base alle tue preferenze di viaggio (${KMtoTravel}Km da percorrere) e età anagrafica inserita: (${youAge }) anni,<br>
-    Siamo lieti di proporle la nostra miglior proposta.</p>
-    <hr>
-    <p class="prince">Prezzo del biglietto: ${grossPrice} €<p>
-    <p class="discount"> Sconto applicato: ${discount}%</p>
-    <hr>
-    <p class="total-prince"> Totale da pagare: ${totalPrince} €<p>
-    `;
-        }
-    // sconto over
-    else if  (youAge  >= 18)
-        {
-            totalPrince = grossPrice - ((grossPrice / 100) * 40);
-            totalPrince = Math.round((totalPrince + Number.EPSILON) * 100) / 100;
-            console.log(totalPrince);
-            discount = 40;
-    document.getElementById ('prince-table').innerHTML = 
-    
-    `
-    <p class="intro"">Ciao, in base alle tue preferenze di viaggio (${KMtoTravel}Km da percorrere) e età anagrafica inserita: (${youAge }) anni,<br>
-    Siamo lieti di proporle la nostra miglior proposta.</p>
-    <hr>
-    <p class="prince">Prezzo del biglietto: ${grossPrice} €<p>
-    <p class="discount"> Sconto applicato: ${discount}%</p>
-    <hr>
-    <p class="total-prince"> Totale da pagare: ${totalPrince} €<p>
-    `;        
-    }
-}
-
-// # 7
-
+BtnConfirm.addEventListener('click', function() {
+    const NameSurnameValue = InputNameSurname.value.trim();
+    const KmValue = parseInt(InputKm.value);
+    const AgeValue = InputAge.value;
+})
 
 
 
